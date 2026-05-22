9a6dda7142e11bed358de472d55d589a74bdb03f
Bernhard Posselt <bernhard@posselt.at>
2026-05-22 11:45:26 +0200

    move to gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "pf2e-return-of-the-runelords-map-remake"
+
+// TODO: remove this later on when plugin is published
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenLocal()
+    }
+}
