6842544f4e3c8209d9d0059531c3e81544555a8f
Bernhard Posselt <bernhard@posselt.at>
2026-09-05 18:03:45 +0200

    update

--- dist/main.mjs
+++ dist/main.mjs
@@ -19,27 +19,14 @@ async function readaloudText(section, behavior, token) {
     await Promise.all(todos);
 }
 
-function sceneData() {
-    return game.scenes.active.getFlag('pf2e-return-of-the-runelords-map-remake', 'readaloud');
-}
-
-async function setSceneRead(value) {
-    return await game.scenes.active.setFlag('pf2e-return-of-the-runelords-map-remake', 'readaloud', {
-        onload: value,
-    });
-}
-
 async function playSceneText() {
-    const data = await fetch(resolvePath()).then(a => a.json());
-    if ('events' in data && 'onload' in data.events && sceneData()?.onload !== true && game.users.activeGM.isSelf) {
-        const confirmed = await Dialog.confirm({
-            title: "Play Readaloud Text",
-            defaultYes: true
-        });
-        if (confirmed) {
+    try {
+        const data = await fetch(resolvePath()).then(a => a.json());
+        if ('events' in data && 'onload' in data.events && game.users.activeGM.isSelf) {
             await readaloudText(data.events.onload);
             await setSceneRead(true);
         }
+    } catch (e) {
     }
 }
 
@@ -48,9 +35,8 @@ Hooks.on('init', () => {
         readaloudText,
         resetSceneRead: () => setSceneRead(false),
     };
-})
-
-
-Hooks.on('canvasReady', async () => {
-    playSceneText()
-})
+    Hooks.on('updateScene', (document, changes) => {
+        if (!changes.active) return;
+        Hooks.once('canvasReady', () => playSceneText())
+    })
+});
